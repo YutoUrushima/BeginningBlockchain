39a29ef3f0822901553b98d1becbb8cb7391c0c5
YutoUrushima <yuto259233@gmail.com>
2022-12-08 23:28:57 +0900

    Add keypairs

--- btc.js
+++ btc.js
@@ -1,3 +1,20 @@
 const btc = require('bitcoinjs-lib');
 const network = btc.networks.testnet;
 const blockCypherTestnetApiEndpoint = 'https;//api.blockcypher.com/v1/btc/test3/';
+
+const getKeys = function () {
+  const aliceKeys = btc.ECPair.makeRandom({
+    network: network
+  });
+  const bobKeys = btc.ECPair.makeRandom({
+    network: network
+  });
+
+  const alicePublic = aliceKeys.getAddress();
+  const alicePrivate = aliceKeys.toWIF();
+  const bobPublic = bobKeys.getAddress();
+  const bobPrivate = bobKeys.toWIF();
+  console.log(alicePublic, alicePrivate, bobPublic, bobPrivate);
+}
+
+getKeys();
